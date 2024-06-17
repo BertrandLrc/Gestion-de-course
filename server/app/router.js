203fdca4792c468b669cac38cf15b9ca3f5ee256
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-06-17 15:34:26 +0200

    mise en place route backend

--- server/app/router.js
+++ server/app/router.js
@@ -7,16 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemActions module for handling item-related operations
-const itemActions = require("./controllers/itemActions");
+const productActions = require("./controllers/productActions");
 
 // Route to get a list of items
-router.get("/items", itemActions.browse);
+router.get("/products", productActions.browse);
 
-// Route to get a specific item by ID
-router.get("/items/:id", itemActions.read);
 
-// Route to add a new item
-router.post("/items", itemActions.add);
+
 
 /* ************************************************************************* */
 
